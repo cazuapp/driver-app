868a4584bcc10e3b98dfdf5bee48e03e40e3d449
Carlos Ferry <cferry@beryldb.com>
2023-07-26 21:09:04 -0400

    Auth bloc.

--- lib/main.dart
+++ lib/main.dart
@@ -9,4 +9,19 @@ Future<void> main() async {
 }
 
 
+class App extends StatelessWidget {
+  final AppInstance instance;
+
+  const App({super.key, required this.instance});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthenticationBloc(instance: instance)),
+      ],
+      child: const AppView(),
+    );
+  }
+}
 
